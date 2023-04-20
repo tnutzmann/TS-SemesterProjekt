e49fe1ad7e890b36e7f80154685477d899efdb41
tnutzmann <tonynutzmann@icloud.com>
2023-04-20 16:37:37 +0200

    simple tcp server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello World!')
+import * as net from 'net'
+import * as wp from 'workerpool'
+
+const PORT = 3000
+const IP = '127.0.0.1'
+const BACKLOG = 100
+
+const workerpool = wp.pool()
+
+console.log('Server start')
+
+net.createServer().listen(PORT, IP, BACKLOG)
+    .on('connection', socket => {
+        console.log(`new connection from ${socket.remoteAddress}:${socket.remotePort}`)
+        socket.on('data', data => {
+            console.log(`sended data: ${data.toString()}`)
+
+            workerpool.exec((str: string)=> {
+                return str
+            }, [data.toString()])
+                .then(res => {
+                socket.write(res + '\n')
+                socket.end()
+            })
+        })
+    })
