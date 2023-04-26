96281e3d442e1752af5168cf6c00db800104903b
tnutzmann <tonynutzmann@icloud.com>
2023-04-26 23:18:18 +0200

    basics.ts created

---
+++ src/basics.ts
@@ -0,0 +1,22 @@
+export const CRLF = "\r\n"
+export const CR = "\r"
+export const LF = "\n"
+
+// the Datatype you can serialise with RESP
+export type RESP_Data = number | null | string | RESP_Data[] // RESP_DATA could be an array of RESP_Data
+
+// Datatypeprefix from the RESP documentation
+export enum RespPrefix {
+    SimpleString = '+', // +OK\r\n
+    BulkString = '$', // $5\r\nhello\r\n
+    Integer = ':', // :1000\r\n
+    Array = '*', // *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
+    Error = '-' // -Error message\r\n
+}
+
+// helper to return some RESP_Date and an offset value
+// (works like a Tuple in Python)
+export type RESP_Segment = {
+    value: RESP_Data
+    offset: number
+}
--- src/resp_decoder.ts
+++ src/resp_decoder.ts
@@ -1,26 +1,4 @@
-const CRLF = "\r\n"
-const CR = "\r"
-const LF = "\n"
-
-// the Datatype you can serialise with RESP
-export type RESP_Data = number | null | string | RESP_Data[] // RESP_DATA could be an array of RESP_Data
-
-// Datatypeprefix from the RESP documentation
-enum RespPrefix {
-    SimpleString = '+', // +OK\r\n
-    BulkString = '$', // $5\r\nhello\r\n
-    Integer = ':', // :1000\r\n
-    Array = '*', // *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
-
-Error = '-' // -Error message\r\n
-}
-
-// helper to return some RESP_Date and an offset value
-// (works like a Tuple in Python)
-type RESP_Segment = {
-    value: RESP_Data
-    offset: number
-}
+import {CR, LF, CRLF, RESP_Data, RespPrefix, RESP_Segment} from "./basics"
 
 export function decodeRESP(buffer: Buffer): RESP_Data {
     try {
@@ -48,7 +26,7 @@ function parse(buffer: Buffer, offset=0): RESP_Segment {
         const error = decodeError(buffer, offset)
         throw new Error(String(error))
     } else {
-        throw new Error(`RESP decode error: unknown prefix -> ${prefix}.`);
+        throw new Error(`RESP decode error: unknown prefix -> "${prefix}".`);
     }
 }
 
