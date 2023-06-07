ab3d97376b8f118d8013f06fa74d50c26289903f
tnutzmann <tonynutzmann@icloud.com>
2023-06-07 09:29:23 +0200

    datahandling reset value by use of undefined

--- src/datahandling.ts
+++ src/datahandling.ts
@@ -2,8 +2,13 @@ import {RESP_Data} from "./globals";
 
 const redis_data: Map<string, RESP_Data> = new Map<string, string>()
 
-export function setData(key: string, value: RESP_Data):void {
-    redis_data.set(key, value);
+export function setData(key: string, value: RESP_Data | undefined):void {
+    if(value === undefined) {
+        redis_data.delete(key)
+    } else {
+        redis_data.set(key, value);
+    }
+
 }
 
 export function getData(key: string): RESP_Data | undefined {
